600f0988cfbf8972994668cb752a208b68d8626b
shubham-kadtan <shubhamkadtan99@gmail.com>
2025-07-08 20:08:18 +0530

    Delete sql_queries directory

--- sql_queries/default_rate_by_grade.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  grade,
-  COUNT(*) AS total_loans,
-  SUM(CASE WHEN loan_status = 'charged off' THEN 1 ELSE 0 END) AS defaults,
-  ROUND(100.0 * SUM(CASE WHEN loan_status = 'charged off' THEN 1 ELSE 0 END) / COUNT(*), 2) AS default_rate
-FROM loan_data
-GROUP BY grade;
